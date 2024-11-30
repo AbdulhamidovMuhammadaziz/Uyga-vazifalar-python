d67ca2188fa0369f33af5f71eeb8eed38f5c714e
Muhammadaziz <github215.1@gmail.com>
2024-11-30 14:38:14 +0500

    100$ = 300$
    100$ oyiga 0.04 $ oshsa necha oyda 300$ bolishi

---
+++ 100$.py
@@ -0,0 +1,24 @@
+# Depozit = 100  
+# Foiz = 0.04  
+# Foyda = 300
+
+
+# t = 0
+# som = Depozit
+
+# while som < Foyda:
+#     som *= (1 + Foiz)  
+#     t += 1  
+
+# print(t)
+
+
+
+# a = int(input("Son kiriting:  "))
+# summa = ''
+# while a >= 1:
+#    qoldiq = a % 2 
+#    summa = str(qoldiq) + summa
+#    a //= 2
+# print(summa)
+
